59fcfb83db89f070b6f718229e2dd7dfaa945937
mauro1892 <mauro.river92@gmail.com>
2021-10-03 00:52:08 -0300

    Added routes

--- app.js
+++ app.js
@@ -5,28 +5,6 @@ const port = process.env.PORT || 3030;
 const routerHome = require('./routes/routeHome');
 const routerUser = require('./routes/routeUser');
 
-<<<<<<< HEAD
-app.listen(port,()=>{
-    console.log("localhost:" + port + " " + "Activo")});
-
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname,'/views/login.html'))})
-
-app.get('/productDetail',function (req, res){
-    res.sendFile(path.join(__dirname,'/views/productDetail.html'))});
-
-app.get('/productList',function (req, res){
-    res.sendFile(path.join(__dirname,'/views/productList.html'))})
-
-app.get('/productCart',function (req, res){
-    res.sendFile(path.join(__dirname,'/views/productCart.html'))});
-
-app.get('/register', function (req,res){
-    res.sendFile(path.join(__dirname,'/views/register.html'))});
-
-app.get('/login', function (req,res){
-    res.sendFile(path.join(__dirname,'/views/login.html'))});
-=======
 app.use(express.static('public'));
 
 app.listen(port,()=>{
@@ -36,4 +14,3 @@ app.listen(port,()=>{
 
 app.use('/', routerHome);
 app.use('/user', routerUser);
->>>>>>> f21d7322dd1bbd48bb8caafadeffa8713a2c7514
