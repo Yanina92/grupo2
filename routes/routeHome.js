8c4e41da85aa1b503e96968e2f2ef006429ee0cd
Braian Nicolás Rivero <nicolas.rivero94@gmail.com>
2021-10-08 21:04:35 -0300

    pag. addProduct editeProduct

--- routes/routeHome.js
+++ routes/routeHome.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controller = require('../controller/controllerHome');
 
 router.get('/', controller.index);
+router.post('/',controller.index);
 
 module.exports = router;
