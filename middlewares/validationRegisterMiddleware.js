bb2ec681381e9a60e2e861cb008e29b6439c8469
SofiaSoraire <sofiasoraire13@gmail.com>
2022-01-12 08:25:47 -0300

    descripcion-validaciones

--- middlewares/validationRegisterMiddleware.js
+++ middlewares/validationRegisterMiddleware.js
@@ -1,3 +1,5 @@
+const express= require('express');
+const router= express.Router();
 const {body} =require('express-validator');
 const path = require('path');
 
@@ -9,19 +11,21 @@ const validations = [
         .isEmail().withMessage('Tienes que escribir un formato de email válido'),
     body('phone').notEmpty().withMessage('Tienes que escribir un telefono'),
     body('password').notEmpty().withMessage('Tienes que escribir una contraseña'),
-    body('image').custom((value,{req}) => {
+    body('image').custom((value, { req }) => {
         let file = req.file;
-        let accepptedExtensions = ['.jpg','.gif','.png'];
-        if (!file){
+        let acceptedExtensions = ['.jpg', '.jpeg', '.gif', '.png'];
+        
+
+        if (!file) {
             throw new Error('Tienes que subir una imagen');
         }else{
             let fileExtension = path.extname(file.originalname);
-            if (!accepptedExtensions.includes(fileExtension)) {
-                throw new Error(`Las extensiones de archivo permitidas son ${acceptedExtensions.join(", ")}`);
+            if (!acceptedExtensions.includes(fileExtension)) {
+                throw new Error(`Las extensiones de archivo permitidas son ${acceptedExtensions.join(', ')}`);
             }
         }
         return true;
-    }),
+    })
 ]
 
 module.exports = validations;
